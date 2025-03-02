96389e99ac3a7558bc6e8b9c32dc4aa24c5045ef
Rashid Enahora <rashidenahora@MacBookPro.attlocal.net>
2025-03-01 23:37:00 -0500

    Added perspective bar and changed the DOM order

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import styles from './ToggleButton.module.css';
 
+/* Make sure to add radio buttons for better accessibility for keyboard users!!! 
+Make sure to implement radio buttons on Wave 2 of doing this workshop!!! */ 
+
 const ToggleButton = ({
   label,
   color,
@@ -22,4 +25,25 @@ const ToggleButton = ({
   );
 };
 
-export default ToggleButton;
+export default ToggleButton; 
+/* The ToggleButton Component will be imported 
+into the ControlPane Component and embedded into the 
+ButtonRow Component */ 
+
+/* Code Snippet */
+
+/* <ButtonRow>
+        {options.map(({ id, label, color, children }) => (
+          <ToggleButton
+            key={id}
+            isSelected={currentOption === id}
+            onClick={() => handleSelectOption(id)}
+            label={label}
+            color={color}
+          >
+            {children}
+          </ToggleButton>
+        ))}
+      </ButtonRow>
+ */ 
+
